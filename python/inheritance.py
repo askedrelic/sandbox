2291af0f4a63d95ba53f784a1877ffbacd21dc5f
Matt Behrens <askedrelic@gmail.com>
2015-01-02 10:37:13 -0800

    add some more examples for private inheritance

--- python/inheritance.py
+++ python/inheritance.py
@@ -7,16 +7,47 @@ class Base(object):
 
 class ChildA(Base):
     def __init__(self):
-        print 'pre a init'
+        print 'pre ChildA init'
         super(ChildA, self).__init__()
-        print 'post a init'
+        print 'post ChildA init'
 
 class ChildB(ChildA):
     def __init__(self):
-        print 'pre b init'
+        print 'pre ChildB init'
         super(ChildB, self).__init__()
-        print 'post b init'
+        print 'post ChildB init'
 
+print 'start'
 print ChildA()
 print
 print ChildB()
+print 'finish'
+
+class BasePrivate(object):
+    def __init__(self):
+        print "Base init"
+
+    def _private(self):
+        print 'Base private'
+
+class ChildA(BasePrivate):
+    def __init__(self):
+        print 'pre ChildA init'
+        super(ChildA, self).__init__()
+        print 'post ChildA init'
+
+    def _private(self):
+        print 'pre ChildA private'
+        super(ChildA, self)._private()
+        print 'post ChildA private'
+
+class ChildB(BasePrivate):
+    pass
+
+class ChildC(BasePrivate):
+    pass
+
+print
+print 'start 2'
+print ChildA()._private()
+print 'finish 2'
